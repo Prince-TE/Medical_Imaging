0bdd4d5a4e2d21dee02ab2f758d215f0bc1d2215
EltonDcunha <152952410+EltonDcunha@users.noreply.github.com>
2024-07-17 12:40:22 +0530

    Adding String constants for Landing and Patient enrollment flow (#3)
    * Adding POM files
    
    * Adding String Constants
    
    * Adding POM files (#2) (#4)
    
    Added POM files for Home Screen and Patient Enrollment flow.

---
+++ src/main/java/stringConstants/PatientEnrollment.java
@@ -0,0 +1,22 @@
+package stringConstants;
+
+public class PatientEnrollment {
+	
+	public static final String patientInformation_Text = "Patient Information";
+	public static final String verifyDetails_Text = "Please verify your details";
+	public static final String ohipErrorMessage = "OHIP format is not valid";
+	public static final String mrnErrorMessage = "MRN format is not valid";
+	public static final String contactInfomation_Text = "Contact Information";
+	public static final String verifyContactDetails_Text = "Please verify your contact details and create password";
+	public static final String note_Text = "Note: The password must be at least 8 characters long and should include at least one upper case letter, one lower case letter, one numeric digit and one special character.";
+	public static final String OBSP_Text = "Ontario Breast Screening Program (OBSP) Eligibility Assessment";
+	public static final String questionDisclaimer_Text = "Please take a moment to answer these questions to help us understand your eligibility for the OBSP Program and determine which pathway best suits your needs. Your responses will assist us in providing you with the most personalized and effective care.";
+	public static final String question1_Text = "1. Have you ever had breast cancer or received a breast cancer diagnosis?*";
+	public static final String question2_Text = "2. Is there a history of breast cancer in your family?*";
+	public static final String question3_Text = "3. Have you had a mammogram in the past 11 months?*";
+	public static final String question4_Text = "4. Have you undergone mastectomy?*";
+	public static final String question5_Text = "5. Have you been tested for the BRCA1/BRCA2 gene, and if so, do you know the results of the test?*";
+	public static final String question6_Text = "6. Have you ever used hormone therapy to manage menopause symptoms or other health conditions?*";
+	public static final String question7_Text = "7. Have you ever undergone fertility treatment known as In Vitro Fertilization (IVF)?*";
+
+}
