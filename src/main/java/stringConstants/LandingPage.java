208ca371bd8c6c4b6e3f68f3cb749fa15d669d17
EltonDcunha <elton.dcunha@revealhealthtech.com>
2024-07-17 12:40:47 +0530

    Adding String Constants

---
+++ src/main/java/stringConstants/LandingPage.java
@@ -0,0 +1,18 @@
+package stringConstants;
+
+public class LandingPage {
+	
+	public static final String activePreventionProgram_titleText = "Active Prevention Program";
+	public static final String welcomeHeader_titleText = "Active Prevention Program";
+	public static final String visionHeader_titleText = "Our Vision";
+	public static final String cardHeader1_titleText = "Enhanced Wellness Paths";
+	public static final String cardHeader2_titleText = "Simplified Coordination";
+	public static final String cardHeader3_titleText = "Proactive Care";
+	public static final String contactInformation_Text = "CONTACT INFORMATION";
+	public static final String addressText = "Address";
+	public static final String followUsOn_Text = "Follow us on";
+	public static final String emailPlaceholderText = "Enter your email id";
+	public static final String passwordPlaceholderText = "Enter your password";	
+	public static final String InValid_Credentials_ErrorMessage = "Invalid credentials";
+
+}
